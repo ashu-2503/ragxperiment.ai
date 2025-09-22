3379b099cf35f6d5b02d59cce59fbf0daa99a5bb
ChinmayEng <chinmay.g@vervetronics.com>
2025-09-22 17:06:09 +0530

    added company logo in sidebar

--- frontend/src/components/layout/Sidebar.tsx
+++ frontend/src/components/layout/Sidebar.tsx
@@ -2,8 +2,21 @@ import React from "react";
 
 const Sidebar: React.FC = () => {
   return (
-    <aside className="bg-dark text-white p-3" style={{ width: "250px" ,paddingRight:"40px"}}>
-      <ul className="nav flex-column ">
+    <aside
+      className="bg-dark text-white p-3 d-flex flex-column"
+      style={{ width: "250px" }}
+    >
+      {/* Company Logo */}
+      <div className="text-center mb-4">
+        <img
+          src="https://www.vervetronics.com/wp-content/uploads/2023/04/cropped-VT_Logo-1.png"
+          alt="Company Logo"
+          style={{ maxWidth: "150px", height: "auto" }}
+        />
+      </div>
+
+      {/* Menu */}
+      <ul className="nav flex-column">
         <li className="nav-item mb-2">
           <a href="#" className="nav-link text-white">Dashboard</a>
         </li>
